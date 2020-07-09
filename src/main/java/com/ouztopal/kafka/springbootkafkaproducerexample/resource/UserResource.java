3b6f08ab6100d74faca234013e458e981d1e8e34
OGUZ.TOPAL <oguz.topal@etiya.com>
2020-07-09 13:58:47 +0300

    deneme

--- src/main/java/com/ouztopal/kafka/springbootkafkaproducerexample/resource/UserResource.java
+++ src/main/java/com/ouztopal/kafka/springbootkafkaproducerexample/resource/UserResource.java
@@ -1,5 +1,28 @@
 package com.ouztopal.kafka.springbootkafkaproducerexample.resource;
 
+
+import com.ouztopal.kafka.springbootkafkaproducerexample.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
 public class UserResource {
 
+    @Autowired
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    private static final String TOPIC = "Kafka_Example";
+
+
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name){
+        User user = new User(name,"tech",15000L);
+        kafkaTemplate.send(TOPIC,user);
+        return "Pubished succes";
+    }
 }
